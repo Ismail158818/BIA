08a870cc501fd1b833a74571f43d1a5eed607654
reeda3943309-design <reeda3943309@gmail.com>
2026-05-05 18:52:45 +0300

    Add Chromosome and RecommendationGA classes

---
+++ Engine/Chromosome.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecommendationSystemAI.Engine
+{
+   
+    public class Chromosome
+    {
+        public List<int> ProductIds { get; set; }
+        public double FitnessScore { get; set; }
+
+        public Chromosome()
+        {
+            ProductIds = new List<int>();
+            FitnessScore = 0;
+        }
+    }
+}
